caca2bd659fe438f7de20d201c97090cb196c40e
Chris <Chris@10.0.0.3>
2014-10-12 17:23:58 -0400

    Removed Base64 Package that was unused

---
+++ RSA Encryptor/src/rsa/encryptor/Euclid.java
@@ -0,0 +1,218 @@
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package rsa.encryptor;
+
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Scanner;
+//import java.util.Base64;
+import java.io.*;
+
+/**
+ * Driver for the RSA encryption class
+ * <p>
+ * Command line interface for generating 2048-bit RSA keys, encrypting messages,
+ * and decrpyting messages. Support for inputing a public key or retriving one's
+ * private key from a file. Support for encrpyting a large messagefile.
+ *
+ * @author David Etler
+ * @author Chris Etler
+ *
+ */
+public class Euclid {
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        Scanner scan = new Scanner(System.in);
+        File keyFile = new File("key.txt");
+        BufferedReader keyReader;
+        PrintWriter keyWriter;
+        File messageFile = new File("message.txt");
+        BufferedReader messageReader;
+        PrintWriter messageWriter;
+        String option = "";
+        BigInteger e = BigInteger.valueOf(65537);
+        BigInteger[] key;
+        BigInteger p;
+        BigInteger q;
+        BigInteger pq;
+        BigInteger d;
+        BigInteger value;
+        BigInteger M;
+        BigInteger C;
+        BigInteger Mn;
+        String messageText;
+        final int RADIX = Character.MAX_RADIX;
+        Message textConverter = new Message();
+        final String COMMANDS = "e  -\tEncrypt\n"
+                + "d  -\tDecrypt\n"
+                + "k  -\tGenerate Key and write to file\n"
+                + "q  -\tQuit\n"
+                + "o  -\tOptions\n"
+                + "ef -\tEncrypt with key from file\n"
+                + "df -\tDecrypt with key from file\n"
+                + "rk -\tRead keys from file\n"
+                + "wm -\tWrite a message to the file\n"
+                + "em -\tEncrypt the message file with the key file\n"
+                + "dm -\tDecypt the message file with the key file\n"
+                + "rm -\tRead the message file";
+
+        System.out.println("TopSEEKRUT Encyptor v3.14159");
+        //Print options
+        System.out.println(COMMANDS);
+        //Key going until user wants to quit
+        while (!option.equals("q")) {
+            System.out.print("Option: ");
+            option = scan.next();
+            scan.nextLine();
+            switch (option) {
+                case "q":
+                    //Closes the program
+                    System.out.println("Closing");
+                    return;
+                case "o":
+                    //Prints the options
+                    System.out.println(COMMANDS);
+                    break;
+                case "k":
+                    //Generates two prime numbers
+                    //Product of numbers is public key
+                    //Modular inverse of e to the product is the private key
+                    //Encrpytion relies on the modular inverse being hard to find
+                    keyWriter = new PrintWriter(keyFile, "UTF-8");
+                    key = RSA.generateKey();
+                    pq = key[0];
+                    d = key[1];
+                    //pq = p.multiply(q);
+                    //d = RSA.decryptionKey(p, q);
+                    keyWriter.println(pq.toString(RADIX));
+                    keyWriter.println(d.toString(RADIX));
+                    keyWriter.close();
+                    System.out.println("Key written to file");
+                    break;
+                case "e":
+                    //Prompts the user for the public key
+                    //Promprs the user for a message
+                    //Returns the encypted message
+                    System.out.print("Enter public key: ");
+                    pq = new BigInteger(scan.next(), RADIX);
+                    scan.nextLine();
+                    System.out.print("Enter message: ");
+                    messageText = scan.nextLine();
+                    M = textConverter.toInt(messageText);
+                    C = RSA.encrypt(M, e, pq);
+                    System.out.println("Encrypted: " + C.toString(RADIX));
+                    break;
+                case "d":
+                    //Prompts the user for a public key
+                    //Prompts the user for the corresspoding private key
+                    //Prompts the user for the encrpyted message
+                    //Decrypts and prints the message
+                    //Both public and private keys are necessary
+                    System.out.print("Enter public key: ");
+                    pq = new BigInteger(scan.next(), RADIX);
+                    System.out.print("Enter private key: ");
+                    d = new BigInteger(scan.next(), RADIX);
+                    System.out.print("Enter encrypted message: ");
+                    C = new BigInteger(scan.next(), RADIX);
+                    M = RSA.decrypt(C, d, pq);
+                    System.out.println("Decrypted message: " + textConverter.toString(M));
+                    break;
+                case "ef":
+                    //Uses the key from the file for encryption
+                    //First key is the public key
+                    //Then asks for a message and prints it
+                    keyReader = new BufferedReader(new FileReader(keyFile));
+                    pq = new BigInteger(keyReader.readLine(), RADIX);
+                    System.out.print("Enter message: ");
+                    messageText = scan.nextLine();
+                    M = textConverter.toInt(messageText);
+                    C = RSA.encrypt(M, e, pq);
+                    System.out.println("Encrypted: " + C.toString(RADIX));
+                    break;
+                case "df":
+                    //Decrypts using both keys from the file
+                    //First is the public, second is the private
+                    //Delimited by newline
+                    keyReader = new BufferedReader(new FileReader(keyFile));
+                    pq = new BigInteger(keyReader.readLine(), RADIX);
+                    d = new BigInteger(keyReader.readLine(), RADIX);
+                    System.out.print("Enter encrypted message: ");
+                    C = new BigInteger(scan.next(), RADIX);
+                    M = RSA.decrypt(C, d, pq);
+                    System.out.println("Decrypted message: " + textConverter.toString(M));
+                    break;
+                case "rk":
+                    //Reads both keys from the key file
+                    //Good for finding your public key
+                    keyReader = new BufferedReader(new FileReader(keyFile));
+                    pq = new BigInteger(keyReader.readLine(), RADIX);
+                    d = new BigInteger(keyReader.readLine(), RADIX);
+                    System.out.println("Public key: " + pq.toString(RADIX));
+                    System.out.println("Private key: " + d.toString(RADIX));
+                    break;
+                case "wm":
+                    //Writes a message to the message file
+                    messageWriter = new PrintWriter(messageFile);
+                    System.out.print("Enter message: ");
+                    messageText = scan.nextLine();
+                    messageWriter.println(messageText);
+                    System.out.println("Message written.");
+                    messageWriter.close();
+                    break;
+                case "rm":
+                    //Reads the message file
+                    messageReader = new BufferedReader(new FileReader(messageFile));
+                    System.out.println("Message: " + messageReader.readLine());
+                    messageReader.close();
+                    break;
+                case "em":
+                    //Encrypts the message file with the key in the key file
+                    messageReader = new BufferedReader(new FileReader(messageFile));
+                    messageText = messageReader.readLine();
+                    messageReader.close();
+                    keyReader = new BufferedReader(new FileReader(keyFile));
+                    pq = new BigInteger(keyReader.readLine(), RADIX);
+                    keyReader.close();
+                    C = RSA.encrypt(messageText, e, pq);
+                    messageWriter = new PrintWriter(messageFile);
+                    messageWriter.println(C.toString(RADIX));
+                    System.out.println("Encrypted message written.");
+                    messageWriter.close();
+                    break;
+                case "dm":
+                    //Decrpyts the message file with the keys in the key file
+                    messageReader = new BufferedReader(new FileReader(messageFile));
+                    messageText = messageReader.readLine();
+                    messageReader.close();
+                    keyReader = new BufferedReader(new FileReader(keyFile));
+                    pq = new BigInteger(keyReader.readLine(), RADIX);
+                    d = new BigInteger(keyReader.readLine(), RADIX);
+                    keyReader.close();
+                    C = new BigInteger(messageText, RADIX);
+                    M = RSA.decrypt(C, d, pq);
+                    messageWriter = new PrintWriter(messageFile);
+                    messageWriter.println(textConverter.toString(M));
+                    System.out.println("Decrypted message written.");
+                    messageWriter.close();
+                    break;
+            }
+        }
+    }
+}
---
+++ RSA Encryptor/src/rsa/encryptor/RSA.java
@@ -0,0 +1,123 @@
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package rsa.encryptor;
+
+import java.lang.Math;
+import java.math.BigInteger;
+import java.util.Random;
+
+/**
+ * RSA Encryption class
+ * <p>
+ * Encrypts a number or string through RSA
+ *
+ * @author David Etler
+ * @author Chris Etler
+ *
+ */
+public class RSA {
+
+    /**
+     * Message class to convert text to integer representation and back.
+     */
+    static final Message textConverter = new Message();
+
+    /**
+     * Encrypts the given integer-represented message.
+     *
+     * @param M message
+     * @param e power
+     * @param pq public key
+     * @return ciphertext
+     */
+    public static BigInteger encrypt(BigInteger M, BigInteger e, BigInteger pq) {
+        BigInteger C = M.modPow(e, pq);
+        return C;
+    }
+
+    /**
+     * Decrypts the given integer-represented message.
+     *
+     * @param C ciphertext
+     * @param d private key
+     * @param pq public key
+     * @return message
+     */
+    public static BigInteger decrypt(BigInteger C, BigInteger d, BigInteger pq) {
+        BigInteger M = C.modPow(d, pq);
+        return M;
+    }
+
+    /**
+     * Encrypts the given string-represented message.
+     *
+     * @param M message
+     * @param e power
+     * @param pq public key
+     * @return ciphertext
+     */
+    public static BigInteger encrypt(String M, BigInteger e, BigInteger pq) {
+        BigInteger Mn = textConverter.toInt(M);
+        return encrypt(Mn, e, pq);
+    }
+
+    /**
+     * Decrypts the given string-represented message.
+     *
+     * @param C ciphertext
+     * @param d private key
+     * @param pq public key
+     * @return message
+     */
+    public static BigInteger decrypt(String C, BigInteger d, BigInteger pq) {
+        BigInteger Cn = textConverter.toInt(C);
+        return encrypt(Cn, d, pq);
+    }
+
+    /**
+     * Generates a 2048-bit public and private key.
+     *
+     * @return {public key, private key}
+     */
+    public static BigInteger[] generateKey() {
+        Random rand = new Random();
+        BigInteger p = BigInteger.probablePrime(2048, rand);
+        BigInteger q = BigInteger.probablePrime(2048, rand);
+        BigInteger[] key = {p.multiply(q), decryptionKey(p, q)};
+        return key;
+    }
+
+    /**
+     * Generates the private key given the prime factors of the public key.
+     *
+     * @param p prime factor 1
+     * @param q prime factor 2
+     * @return private key
+     */
+    public static BigInteger decryptionKey(BigInteger p, BigInteger q) {
+        BigInteger v = p.subtract(BigInteger.valueOf(1)).multiply(q.subtract(BigInteger.valueOf(1)));
+        BigInteger d = Euclidian.extendedEuclidan(BigInteger.valueOf(65537), v)[1];
+        if (d.compareTo(BigInteger.valueOf(0)) < 0) {
+            d = d.add(v);
+        }
+        return d;
+    }
+}
